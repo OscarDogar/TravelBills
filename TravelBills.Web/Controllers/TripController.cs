100ef4e4478c7f9954588332d5134fd607752640
Oscar Doria <oscardoria244956@correo.itm.edu.com>
2020-03-28 16:17:16 -0500

    Added Trip Controller for web part 1

---
+++ TravelBills.Web/Controllers/TripController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TravelBills.Web.Data;
+using TravelBills.Web.Data.Entities;
+
+namespace TravelBills.Web.Controllers
+{
+    public class TripController : Controller
+    {
+        private readonly DataContext _context;
+
+        public TripController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Trip
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Trips.Include(t => t.User).Include(t => t.TripType).Include(t => t.TripDetails).ToListAsync());
+        }
+
+        // GET: Trip/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tripEntity = await _context.Trips
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tripEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(tripEntity);
+        }
+
+        // GET: Trip/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tripEntity = await _context.Trips.FindAsync(id);
+            if (tripEntity == null)
+            {
+                return NotFound();
+            }
+            return View(tripEntity);
+        }
+
+        // POST: Trip/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, TripEntity tripEntity)
+        {
+            if (id != tripEntity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tripEntity);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TripEntityExists(tripEntity.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tripEntity);
+        }
+
+        // GET: Trip/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tripEntity = await _context.Trips
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (tripEntity == null)
+            {
+                return NotFound();
+            }
+
+            _context.Trips.Remove(tripEntity);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TripEntityExists(int id)
+        {
+            return _context.Trips.Any(e => e.Id == id);
+        }
+    }
+}
