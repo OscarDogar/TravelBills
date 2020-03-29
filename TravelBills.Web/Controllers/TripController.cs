d54195b333c85bb78d196f841bd3f3e6ef2339cf
Oscar Doria <oscardoria244956@correo.itm.edu.com>
2020-03-28 20:00:04 -0500

    Added Some features to trip controller V.1.5

--- TravelBills.Web/Controllers/TripController.cs
+++ TravelBills.Web/Controllers/TripController.cs
@@ -7,40 +7,35 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using TravelBills.Web.Data;
 using TravelBills.Web.Data.Entities;
+using TravelBills.Web.Helpers;
+using TravelBills.Web.Models;
 
 namespace TravelBills.Web.Controllers
 {
     public class TripController : Controller
     {
         private readonly DataContext _context;
+        private readonly IConverterHelper _converterHelper;
 
-        public TripController(DataContext context)
+        public TripController(DataContext context, IConverterHelper converterHelper)
         {
             _context = context;
+            _converterHelper = converterHelper;
         }
 
         // GET: Trip
         public async Task<IActionResult> Index()
         {
+            
             return View(await _context.Trips.Include(t => t.User).Include(t => t.TripType).Include(t => t.TripDetails).ToListAsync());
         }
 
         // GET: Trip/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            var suma = _context.TripDetails.Select(x=> x.BillValue).Sum();
 
-            var tripEntity = await _context.Trips
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (tripEntity == null)
-            {
-                return NotFound();
-            }
-
-            return View(tripEntity);
+            return View(await _context.TripDetails.Where(td => td.Trip.Id == id).ToListAsync());
         }
 
         // GET: Trip/Edit/5
@@ -59,9 +54,6 @@ namespace TravelBills.Web.Controllers
             return View(tripEntity);
         }
 
-        // POST: Trip/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, TripEntity tripEntity)
@@ -92,26 +84,6 @@ namespace TravelBills.Web.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(tripEntity);
-        }
-
-        // GET: Trip/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var tripEntity = await _context.Trips
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (tripEntity == null)
-            {
-                return NotFound();
-            }
-
-            _context.Trips.Remove(tripEntity);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
         }
 
         private bool TripEntityExists(int id)
